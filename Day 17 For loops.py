955022f9cfec038959ef0a4f61b11afd00fe2a58
Adarsh Anshuman <adarshanshuman001@gmail.com>
2026-09-12 12:53:46 +0530

    Python 100 Days...Practice

---
+++ Day 17 For loops.py
@@ -0,0 +1,41 @@
+# name = "Adasrh Anshuman"
+# for i in name:
+#     print(i, end=', ')
+
+
+# colors = ('Green', 'Yellow', 'Blue', 'Red')
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+#     if (i=="e"):
+#         print("I like this letter more")
+
+# for k in range (1, 20001):
+#     print(k)
+
+# for k in range (1, 100, 3):
+#     print(k)
+
+# for k in range (1, 9999, 1):
+#     print(k)
+#     if(k == 2641):
+#         print("Phone Unlocked ")
+#     else:
+#         print("Wrong Password ")
+# num = 0
+# for i in range(1,101):
+#     num = num + i
+# print(num)
+
+# for k in range (0, 700, 7):
+#     print(k)
+
+# for i in range(1,11):
+    # print(f"7 x {i} = {7*i}")
+
+for i in range(1,101):
+    if i % 3 ==0:
+        print(f"{i}")
+    else :
+        continue
---
+++ Day 18 While Loops.py
@@ -0,0 +1,16 @@
+# i = 0
+# while (i<=3):
+#     print(i)
+#     i = i + 1
+
+# print("Done with the loop")
+
+count = 5
+while(count > -10):
+    print(count)
+    count = count - 1
+
+# count = 10
+# while(count < 20):
+#     print(count)
+#     count = count + 1
